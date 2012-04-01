54f7f9ab9f169ee584662993b2c9f3d8cb1f58a9
Daniel Pozzi <bonndan76@googlemail.com>
2012-04-01 23:14:44 +0200

    just one script for invocation

--- pythophant.php
+++ pythophant.php
@@ -1,8 +1,11 @@
 <?php
-
+/**
+ * PythoPhant invokation script
+ * 
+ *  
+ */
 require_once dirname(__FILE__) . '/classes/autoload.php';
 
-$filename = $argv[1];
 
 $pp = new PythoPhant();
-echo $pp->convert($filename);
+$pp->main($argv);
