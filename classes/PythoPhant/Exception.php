47a7acffb4cc96c1d391abaab32e3758a92f80d2
Daniel Pozzi <bonndan76@googlemail.com>
2012-04-08 07:24:25 +0200

    PythoPhant_Exception uses code as line number carrier

--- classes/PythoPhant/Exception.php
+++ classes/PythoPhant/Exception.php
@@ -2,10 +2,18 @@
 /**
  * PythoPhant_Exception
  * 
- * 
+ * use the code to pass the line number
  * 
  */
 class PythoPhant_Exception extends Exception
 {
-    
+    /**
+     * returns the code
+     * 
+     * @return type 
+     */
+    public function getSourceLine()
+    {
+        return $this->getCode();
+    }
 }
