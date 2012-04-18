1da0bc201876a585584906a59df15f82e0333392
Daniel Pozzi <bonndan76@googlemail.com>
2012-04-18 22:26:14 +0200

    Integration test of the scalar type hint macro

---
+++ macros/scalarTypeHintException.pp
@@ -0,0 +1,3 @@
+<?php
+if ! is_%1$s(%2$s)
+    throw new InvalidArgumentException: %2$s " is not of type " %1$s
---
+++ tests/macros/scalarTypeHintExceptionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once dirname(dirname(__FILE__)) . '/bootstrap.php';
+
+/**
+ * conversion test
+ * 
+ */
+class ScalarTypeHintExceptionMacroTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * get a configuted macro
+     * 
+     * @return PythoPhant_Macro 
+     */
+    public function getMacro()
+    {
+        $macro = new PythoPhant_Macro();
+        return $macro;
+    }
+    
+    /**
+     * ensures the correct conversion of the scalar type hint exception macro 
+     */
+    public function testConversion()
+    {
+       $macro = $this->getMacro();
+       $filename = dirname(dirname(__DIR__)) . '/macros/scalarTypeHintException.pp';
+       $macro->setSource(file_get_contents($filename));
+       $macro->setParams(array('bool', 'myVar'));
+       
+       $source = $macro->getSource();
+       $factory = new PythoPhant_TokenFactory();
+       $scanner = new PythoPhant_Scanner($factory);
+       
+       $scanner->scanSource($source);
+       $tokenList = $scanner->getTokenList();
+       $this->assertInstanceOf('TokenList', $tokenList);
+       $this->assertEquals(24, $tokenList->count());
+    }
+}
