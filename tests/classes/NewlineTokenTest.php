cc38c1d0ced61563af3bd9663ff1dd979fd7b901
Daniel Pozzi <bonndan76@googlemail.com>
2012-04-27 07:00:10 +0200

    prepared test cases for newline token eol handling

--- tests/classes/NewlineTokenTest.php
+++ tests/classes/NewlineTokenTest.php
@@ -38,4 +38,34 @@ class NewlineTokenTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(PHP_EOL, $token->getContent());
         $this->assertEquals(1, $token->getLine());
     }
+    
+    public function testAffectTokenlistInsertsSemicolonInRegularLines()
+    {
+        $token = NewLineToken::createEmpty(1);
+        $this->assertContains(';', $token->getContent());
+    }
+    
+    public function testAffectTokenlistInsertsOpenBraceIfNextLineIndentedDeeper()
+    {
+        $token = NewLineToken::createEmpty(1);
+        $this->assertContains('{', $token->getContent());
+    }
+    
+    public function testAffectTokenlistInsertsCloseBraceIfNextLineIndentedLess()
+    {
+        $token = NewLineToken::createEmpty(1);
+        $this->assertContains('}', $token->getContent());
+    }
+    
+    public function testAffectTokenlistInsertsNothingIfLineEmpty()
+    {
+        $token = NewLineToken::createEmpty(1);
+        $this->assertEquals(PHP_EOL, $token->getContent());
+    }
+    
+    public function testAffectTokenlistInsertsClosingBracesAtListEnd()
+    {
+        $token = NewLineToken::createEmpty(1);
+        $this->assertContains('}', $token->getContent());
+    }
 }
