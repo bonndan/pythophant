49b8a4f34d92788f1212cf857403d9eb2b1054c0
Daniel Pozzi <bonndan76@googlemail.com>
2012-04-29 12:00:02 +0200

    added long as new returnType to grammar

--- classes/PythoPhant/Grammar.php
+++ classes/PythoPhant/Grammar.php
@@ -74,7 +74,7 @@ abstract class PythoPhant_Grammar
      * @var array 
      */
     public static $returnValues = array(
-        'string', 'int', 'integer', 'float', 'double', 'bool', 'boolean', 'void'
+        'string', 'int', 'integer', 'float', 'double', 'long', 'bool', 'boolean', 'void'
     );
     
     /**
